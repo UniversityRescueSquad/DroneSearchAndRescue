ff387abb63d516bf8cd1d4525db7bb17255b58b5
Iliya Zhechev <iliya.zhechev@gmail.com>
2023-08-24 14:11:01 +0300

    Refactor dataset + init eval notebook

--- drone_sar/ipsar_dataset.py
+++ drone_sar/ipsar_dataset.py
@@ -1,59 +1,16 @@
 import os
 import torch
-import logging
-import numpy as np
 from PIL import Image
 from torch.utils.data import Dataset
 from xml.etree import ElementTree
-from object_detector import ObjectDetector
-from utils import setup_logging
+import numpy as np
 
 
 class IPSARDataset(Dataset):
-    """
-    A PyTorch dataset for the IPSAR dataset.
-
-    Args:
-        root_dir (str): The path to the root directory of the IPSAR dataset.
-        split_type (str): The name of the dataset split ("train" or "test").
-    """
-
-    def __init__(self, root_dir: str, split_type: str):
-        assert split_type in ["train", "test"], f"Invalid set name: {split_type}"
-
-        self.images_path = os.path.join(root_dir, "heridal", split_type + "Images")
-        self.labels_path = os.path.join(self.images_path, "labels")
-        self.dataset = self._create_dataset()
-
-    def _create_dataset(self) -> Dataset:
-        dataset = []
-        for filename in os.listdir(os.path.join(self.images_path)):
-            if filename.endswith(".JPG"):
-                img_path = os.path.join(self.images_path, filename)
-                annotation_path = os.path.join(
-                    self.labels_path, filename.split(".")[0] + ".xml"
-                )
-
-                if os.path.exists(annotation_path):
-                    annotation = self._parse_annotation(annotation_path)
-                    if len(annotation["boxes"]) > 0:
-                        dataset.append(
-                            (
-                                np.asarray(Image.open(img_path)),
-                                {
-                                    "boxes": annotation["boxes"][0],
-                                    "class_labels": annotation["class_labels"][0],
-                                },
-                            )
-                        )
-                    else:
-                        # TODO: Add empty dataset entry
-                        continue
-
-        logging.info(
-            f"{len(dataset)} number of annotated images loaded in {self.images_path}"
-        )
-        return dataset
+    def __init__(self, images_root: str):
+        self.labels_path = os.path.join(images_root, "labels")
+        image_paths = [os.path.join(images_root, f) for f in os.listdir(images_root)]
+        self.image_paths = [path for path in image_paths if os.path.isfile(path)]
 
     def _parse_annotation(self, annotation_path: str) -> dict:
         root = ElementTree.parse(annotation_path).getroot()
@@ -72,17 +29,23 @@ class IPSARDataset(Dataset):
         }
 
     def __getitem__(self, index: int) -> dict:
-        return self.dataset[index]
+        image_path = self.image_paths[index]
+        dir_path, file_name = os.path.split(image_path)
+        file_identifier, ext = file_name.split(".")
+        annotation_path = os.path.join(self.labels_path, f"{file_identifier}.xml")
+        annotation = self._parse_annotation(annotation_path)
+        pixel_values = torch.tensor(np.array(Image.open(image_path)))
+
+        return {
+            "pixel_values": pixel_values,
+            "boxes": annotation["boxes"],
+            "class_labels": annotation["class_labels"],
+        }
 
     def __len__(self) -> int:
-        return len(self.dataset)
+        return len(self.image_paths)
 
 
 if __name__ == "__main__":
-    setup_logging()
-
     train_data = IPSARDataset("drone_sar/data", "train")
     test_data = IPSARDataset("drone_sar/data", "test")
-
-    detector = ObjectDetector()
-    detector.fine_tune(train_data, test_data)
