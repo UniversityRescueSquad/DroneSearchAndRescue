0058c4071eb7ce8d59a44efd01c267546aef37c7
iz <iz@ml01.myxrobotics.com>
2024-02-28 19:12:36 +0200

    restructure dataset class

--- drone_sar/ipsar_dataset.py
+++ drone_sar/ipsar_dataset.py
@@ -4,14 +4,94 @@ from PIL import Image
 from torch.utils.data import Dataset
 from xml.etree import ElementTree
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 
 class IPSARDataset(Dataset):
-    def __init__(self, images_root: str, load_images=True):
-        self.load_images = load_images
+    def __init__(self, images_root: str):
         self.labels_path = os.path.join(images_root, "labels")
         image_paths = [os.path.join(images_root, f) for f in os.listdir(images_root)]
         self.image_paths = [path for path in image_paths if os.path.isfile(path)]
+
+    def __getitem__(self, index: int) -> dict:
+        image_path = self.image_paths[index]
+        dir_path, file_name = os.path.split(image_path)
+        file_identifier, ext = file_name.split(".")
+
+        annotation_path = os.path.join(self.labels_path, f"{file_identifier}.xml")
+        annotation = self._parse_annotation(annotation_path)
+        pil = Image.open(image_path)
+
+        return {
+            "img_path": image_path,
+            "pil": pil,
+            "target_sizes": pil.size,
+            "boxes": annotation["boxes"],
+            "class_labels": annotation["class_labels"],
+        }
+
+    def __len__(self) -> int:
+        return len(self.image_paths)
+
+    @staticmethod
+    def plt_show(item, size=10, im_opacity=0.5):
+        np_pil = np.array(item["pil"])
+
+        fig, ax = plt.subplots(dpi=100, figsize=(size, size))
+        plt.tight_layout()
+        ax.imshow(np_pil, alpha=im_opacity)
+
+        for x1, y1, x2, y2 in item["boxes"]:
+            w = x2 - x1
+            h = y2 - y1
+            ax.add_patch(
+                Rectangle((x1, y1), w, h, fill=None, edgecolor="red", linewidth=1)
+            )
+
+        plt.close()
+
+        return fig
+
+    def get_dataloader(self, processor, bs, shuffle):
+        return torch.utils.data.DataLoader(
+            self,
+            shuffle=shuffle,
+            batch_size=bs,
+            collate_fn=lambda b: self.collate_fn(b, processor),
+        )
+
+    @staticmethod
+    def collate_fn(items, processor):
+        dos = {
+            "do_resize": True,
+            "do_rescale": True,
+            "do_normalize": True,
+            "do_pad": True,
+        }
+        images = [it["pil"] for it in items]
+        sizes = [im.size for im in images]
+        processed_images = processor(images=images, return_tensors="pt", **dos)
+
+        def format_annotation(boxes, W, H):
+            if len(boxes) == 0:
+                return boxes
+            boxes = boxes.clone()
+            boxes[:, 0] /= W
+            boxes[:, 1] /= H
+            boxes[:, 2] /= W
+            boxes[:, 3] /= H
+            return boxes
+
+        labels = [
+            {
+                "boxes": format_annotation(it["boxes"], W, H),
+                "class_labels": torch.zeros(len(it["boxes"]), dtype=torch.long),
+            }
+            for it, (W, H) in zip(items, sizes)
+        ]
+        info = {"target_sizes": sizes}
+        return {"labels": labels, **processed_images}, info
 
     def _parse_annotation(self, annotation_path: str) -> dict:
         if os.path.exists(annotation_path):
@@ -32,33 +112,3 @@ class IPSARDataset(Dataset):
             "boxes": torch.tensor(boxes, dtype=torch.float32),
             "class_labels": torch.ones(len(boxes), dtype=torch.int64),
         }
-
-    def __getitem__(self, index: int) -> dict:
-        image_path = self.image_paths[index]
-        dir_path, file_name = os.path.split(image_path)
-        file_identifier, ext = file_name.split(".")
-        annotation_path = os.path.join(self.labels_path, f"{file_identifier}.xml")
-        annotation = self._parse_annotation(annotation_path)
-        
-        pil = lambda: Image.open(image_path)
-        pixel_values = lambda: torch.tensor(np.array(pil))
-
-        if self.load_images:
-            pil = pil()
-            pixel_values = pixel_values()
-
-        return {
-            "img_path": image_path,
-            "pil": pil,
-            "pixel_values": pixel_values,
-            "boxes": annotation["boxes"],
-            "class_labels": annotation["class_labels"],
-        }
-
-    def __len__(self) -> int:
-        return len(self.image_paths)
-
-
-if __name__ == "__main__":
-    train_data = IPSARDataset("drone_sar/data", "train")
-    test_data = IPSARDataset("drone_sar/data", "test")
