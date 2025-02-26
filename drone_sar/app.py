5019cccb0ea35a6e6ebd444265292cab3e083ee4
Iliya Zhechev <iliya.zhechev@gmail.com>
2025-02-26 17:34:00 +0200

    App WIP

--- drone_sar/app.py
+++ drone_sar/app.py
@@ -1,22 +1,41 @@
-import threading
+import pickle
+from threading import Thread
 import time
 import cv2
 from PIL import Image
 import numpy as np
+from datetime import datetime
 import panel as pn
+import os
+
+import torch
 
 from drone_sar.lightning_detector import LightningDetector
+from drone_sar.vis import draw_prediction
+
+# from drone_sar.lightning_detector import LightningDetector
 
 pn.extension()
 pn.config.throttled = True
-pn.extension(nthreads=4)
+# pn.extension(nthreads=4)
+pn.extension(notifications=True)
+
+DEFAULT_MODEL_PATH = os.path.abspath("./workdir/epoch=65-step=10494.ckpt")
+DEFAULT_INPUT_DIR = os.path.abspath("./workdir/input_dir")
+DEFAULT_OUTPUT_DIR = os.path.abspath("./workdir/output_dir")
 
 
 class VideoIndexable:
     def __init__(self, path) -> None:
         self.path = path
         self.cap = cv2.VideoCapture(path)
-        self.frames_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.frames_count = int(self.fps)
+
+    def get_display_duration(self):
+        duration_in_seconds = self.frames_count / self.fps
+        delta_time = datetime.timedelta(seconds=duration_in_seconds)
+        return str(delta_time)
 
     def __len__(self):
         return self.frames_count
@@ -31,206 +50,214 @@ class VideoIndexable:
             return id, frame
 
 
-def indexable_file_loader(path):
-    ext = path.split(".")[-1].lower()
-    if ext in ["mp4"]:
-        return VideoIndexable(path)
-    raise NotImplementedError()
-
-
-class SimpleSampler:
-    def __init__(self, len) -> None:
-        self.len = len
-
-    def __iter__(self):
-        self.current = 0
-        return self
-
-    def __next__(self):
-        current = self.current
-        if current < self.len:
-            self.current += 1
-            return current
-        raise StopIteration()
-
-
 class PredictionManager:
-    def __init__(self) -> None:
-        frame_indexable = indexable_file_loader("/home/iz/workspace/DJI_0002.MP4")
-        sampler = SimpleSampler(len(frame_indexable))
-        self.model = LightningDetector.load_from_checkpoint(
-            "/home/iz/workspace/DroneSearchAndRescue/.checkpoints/epoch=65-step=10494.ckpt",
-            lr=0.01,
-        )
-        self.subscribers = []
-        self.predictions_state = {}
-        # is_running = False
-        # thread = threading.Thread(target=run)
-        # thread.start()
-
-        # def run():
-        #     while True:
-        #         if is_running:
-        #             pil = frame_indexable[index]
-        #             self.model.predict(pil)
-        #         else:
-        #             time.sleep(500)
-
-        # def start():
-        #     nonlocal is_running
-        #     is_running = True
-
-        # def stop():
-        #     nonlocal is_running
-        #     is_running = False
-
-        self.container = pn.Column()
-
-    def subscribe(self, subscriber):
-        self.subscribers.append(subscriber)
-
-    def force_predict(self, frame_identifier):
-        path, index = frame_identifier
-        frame_identifier, frame = indexable_file_loader(path)[index]
-        prediction = self.model.predict(frame)
-        self.predictions_state[frame_identifier] = prediction
-        self.notify(frame_identifier)
-
-    def get_prediction(self, frame_identifier):
-        if frame_identifier in self.predictions_state:
-            return self.predictions_state[frame_identifier]
-        return None
-
-    def notify(self, frame_identifier):
-        for it in self.subscribers:
-            it.update_prediction(self.predictions_state, frame_identifier)
-
-    def render(self):
-        return self.container
-
-
-class PredictionViewer:
-    def __init__(self, prediction_manager) -> None:
-        self.current_frame_index = pn.widgets.IntSlider(
-            name="Current Frame",
-            value=0,
-            start=0,
-            end=100,
-            width=800,
-        )
-        self.prediction_manager = prediction_manager
-        self.current_frame_identifier = None
-        self.prediction_manager.subscribe(self)
-
-        self.current_frame_container = pn.Column("")
-        next_btn = pn.widgets.Button(name=">", button_type="primary")
-        prev_btn = pn.widgets.Button(name="<", button_type="primary")
-        next_btn.on_click(lambda _: update_frame(frame_offset=1))
-        prev_btn.on_click(lambda _: update_frame(frame_offset=-1))
-
-        next_next_btn = pn.widgets.Button(name=">>", button_type="primary")
-        prev_prev_btn = pn.widgets.Button(name="<<", button_type="primary")
-        next_next_btn.on_click(lambda _: update_frame(frame_offset=5 * 30))
-        prev_prev_btn.on_click(lambda _: update_frame(frame_offset=-5 * 30))
-
-        predict_btn = pn.widgets.Button(name="Predict", button_type="primary")
-        predict_btn.on_click(lambda _: predict_on_current())
-
-        def predict_on_current():
-            if self.current_frame_identifier is not None:
-                self.container.loading = True
-                self.prediction_manager.force_predict(self.current_frame_identifier)
-
-        def update_frame(frame_offset):
-            new_value = np.clip(
-                self.current_frame_index.value + frame_offset,
-                0,
-                len(self.frames_indexable) - 1,
-            )
-            self.current_frame_index.value = new_value
-
-        self.current_frame_index.param.watch(lambda _: self._show(), "value")
-        self.container = pn.Column(
-            predict_btn,
-            self.current_frame_container,
-            pn.Column(
-                self.current_frame_index,
-                pn.Row(prev_prev_btn, prev_btn, next_btn, next_next_btn),
-                styles=dict(background="#eeeeee"),
-            ),
-            styles=dict(background="#eeeeee", padding="25px 15px"),
-        )
-
-    def draw_prediction(self, pil, pred):
-        print("drawing frame", pred)
-        im = np.array(pil)
-        for x0, y0, w, h in pred:
-            x1, y1 = x0 + w, y0 + h
-            x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
-            im = cv2.rectangle(im, (x0, y0), (x1, y1), (255, 0, 0), 5)
-
-        return Image.fromarray(im)
-
-    def _show(self):
-        self.container.loading = True
-        self.current_frame_identifier, frame = self.frames_indexable[
-            self.current_frame_index.value
-        ]
-        prediction = self.prediction_manager.get_prediction(
-            self.current_frame_identifier
-        )
-
-        if prediction is not None:
-            frame = self.draw_prediction(frame, prediction)
-
-        if frame is not None:
-            frame.thumbnail((800, 800))
-
-        self.current_frame_container[0] = frame
-        self.container.loading = False
-
-    def update_frame(self, frame_indexable, index=0):
-        self.frames_indexable = frame_indexable
-        self.current_frame_index.value = index
-        self.current_frame_index.end = len(frame_indexable)
-        self._show()
-
-    def update_prediction(self, all_predictions, frame_identifier):
-        self._show()
-
-    def render(self):
-        return self.container
-
-
-class App:
     def __init__(self):
-        prediction_manager = PredictionManager()
-        video_indexable = VideoIndexable("/home/iz/workspace/DJI_0002.MP4")
-        self.current_mission = PredictionViewer(prediction_manager)
-        self.current_mission.update_frame(video_indexable)
+        self.is_running = False
+        self.prediction_initiated = False
+        self.on_progress = None
+        self.on_done = None
+        self.on_error = None
+        self.on_finally = None
 
-    def get_files_list_container(self):
-        container = pn.Row()
+    def on(self, on_progress, on_done, on_error, on_finally):
+        self.on_progress = on_progress
+        self.on_done = on_done
+        self.on_error = on_error
+        self.on_finally = on_finally
 
-        container.append("Files list")
+    def start(self, **kwargs):
+        self.is_running = True
+        self.prediction_initiated = True
+        self.process = Thread(target=self._run, kwargs=kwargs)
+        self.process.start()
 
-        return container
+    # def stop(self):
+    #     self.process.terminate()
+    #     self.is_running = False
+    #     self.on_finally()
 
-    def render(self):
-        template_container = pn.template.MaterialTemplate(title="UASO - DRONE SAR")
+    def _run(self, model_path, input_file_path, output_dir, device):
+        try:
+            print("> Process start")
+            now = datetime.now()
+            task_name = f"prediction-task-{now}"
+            task_dir = os.path.join(output_dir, task_name)
 
-        files_list_container = self.get_files_list_container()
+            model = LightningDetector.load_from_checkpoint(
+                model_path, lr=0.01, ignore_mismatched_sizes=True
+            ).to(device)
 
-        current_mission_element = self.current_mission.render()
+            target_indexable = VideoIndexable(input_file_path)
+            target_length = len(target_indexable)
 
-        template_container.main.append(files_list_container)
-        template_container.main.append(current_mission_element)
+            print(f"> Number of frames {target_length}")
 
-        return template_container
+            os.makedirs(task_dir, exist_ok=True)
+            with torch.inference_mode():
+                for index in range(0, target_length, 30 * 10):
+                    indexing_result = target_indexable[index]
+                    if indexing_result is not None:
+                        _, frame_pil = indexing_result
+                    prediction_out = model.predict(frame_pil)
+
+                    bbox_pil = draw_prediction(frame_pil, prediction_out)
+                    result_path = os.path.join(task_dir, f"{index}.png")
+                    bbox_pil.save(result_path)
+
+                    prediction_path = os.path.join(task_dir, f"{index}.pkl")
+                    with open(prediction_path, "wb+") as fp:
+                        pickle.dump(
+                            {"output": prediction_out, "frame_index": index}, fp
+                        )
+
+                    percentage = int((index + 1) / target_length * 100)
+                    self.on_progress(percentage)
+            print("> Process done")
+            self.on_done()
+        except Exception as e:
+            self.on_error(e)
+        finally:
+            self.is_running = False
+            self.on_finally()
+
+
+def handle_reload_input_dir(input_dir, target_file_selector):
+    target_file_selector.loading = True
+    target_file_selector.disabled = True
+    try:
+        files = os.listdir(input_dir)
+        valid_files = [f for f in files if f.lower().endswith(".mp4")]
+        target_file_selector.options = valid_files
+        target_file_selector.value = valid_files[0]
+        target_file_selector.disabled = False
+    except Exception as e:
+        pn.state.notifications.error("Invalid input dir!", duration=1000)
+    finally:
+        target_file_selector.loading = False
+
+
+def handle_start_prediction(
+    controls_container,
+    ongoing_prediction_container,
+    prediction_manager,
+    model_path_el,
+    input_dir_el,
+    output_dir_el,
+    target_file_selector,
+    device_selector_el,
+):
+    controls_container.loading = True
+    ongoing_prediction_container.visible = True
+    input_file_path = os.path.join(input_dir_el.value, target_file_selector.value)
+    prediction_manager.start(
+        model_path=model_path_el.value,
+        input_file_path=input_file_path,
+        output_dir=output_dir_el.value,
+        device=device_selector_el.value,
+    )
+
+
+def handle_end_prediction(controls_container):
+    controls_container.loading = False
+
+
+def handle_prediction_success(result_el):
+    result_el.clear()
+    result_el.append(
+        pn.pane.Alert("### Prediction finished successfully", alert_type="success")
+    )
+
+
+def handle_prediction_error(e, result_el):
+    result_el.clear()
+    result_el.append(
+        pn.pane.Alert(f"### Error occurred during prediction {e}", alert_type="danger")
+    )
+
+
+def handle_prediction_update(percent, progress_element):
+    progress_element.value = percent
+
+
+prediction_manager = PredictionManager()
+
+
+def render_app():
+    template_container = pn.template.MaterialTemplate(
+        title="⛑️ University Rescue Squad - DRONE SAR"
+    )
+
+    model_path_el = pn.widgets.TextInput(value=DEFAULT_MODEL_PATH, name="Path to model")
+    input_dir_el = pn.widgets.TextInput(value=DEFAULT_INPUT_DIR, name="Input directory")
+    skip_frames_el = pn.widgets.TextInput(value=30 * 5, name="Frames to skip")
+    output_dir_el = pn.widgets.TextInput(
+        value=DEFAULT_OUTPUT_DIR, name="Output directory"
+    )
+
+    target_file_selector = pn.widgets.Select(name="Input target video")
+    input_dir_el.param.watch(
+        lambda e: handle_reload_input_dir(input_dir_el.value, target_file_selector),
+        "value",
+    )
+    handle_reload_input_dir(input_dir_el.value, target_file_selector)
+
+    device_selector_el = pn.widgets.Select(options=["cpu", "cuda", "mps"], value="cpu")
+
+    prediction_progress_el = pn.indicators.Progress(name="Prediction Progress", value=0)
+    prediction_result_el = pn.Column()
+
+    ongoing_prediction_container = pn.Column(
+        "**Ongoing prediction**",
+        prediction_progress_el,
+        prediction_result_el,
+    )
+    ongoing_prediction_container.visible = prediction_manager.prediction_initiated
+
+    start_prediction_btn = pn.widgets.Button(name="Start Prediction")
+    start_prediction_btn.on_click(
+        lambda e: handle_start_prediction(
+            controls_container,
+            ongoing_prediction_container,
+            prediction_manager,
+            model_path_el,
+            input_dir_el,
+            output_dir_el,
+            target_file_selector,
+            device_selector_el,
+        )
+    )
+
+    prediction_manager.on(
+        lambda p: handle_prediction_update(p, prediction_progress_el),
+        lambda: handle_prediction_success(prediction_result_el),
+        lambda e: handle_prediction_error(e, prediction_result_el),
+        lambda: handle_end_prediction(controls_container),
+    )
+
+    controls_container = pn.Column(
+        "**Configurable parameters**",
+        model_path_el,
+        input_dir_el,
+        output_dir_el,
+        target_file_selector,
+        device_selector_el,
+        start_prediction_btn,
+        width=600,
+    )
+    controls_container.loading = prediction_manager.is_running
+
+    prediction_container = pn.Column(
+        controls_container,
+        ongoing_prediction_container,
+    )
+
+    template_container.main.append(prediction_container)
+
+    return template_container
 
 
 pn.serve(
-    {"/": App().render()},
-    port=8888,
+    {"/": render_app},
+    port=8887,
     show=False,
 )
